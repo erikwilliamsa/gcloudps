ff59a000e86ebeed72c2a69891576c3a26f1b97e
erikwilliamsa <erikwilliamsa@gmail.com>
2017-10-20 13:30:17 -0500

    Work on sub worker

--- workers/subscriber.go
+++ workers/subscriber.go
@@ -1,13 +1,19 @@
 package workers
 
-//Consumes until toggled off
-func Consume(toggle <-chan struct{}) int {
+import (
+	ps "github.com/erikwilliamsa/gcloudps/pubsub"
+)
+
+// ConsumeCount until toggled off.
+// It returns the number of messages consumed.
+func ConsumeCount(toggle <-chan struct{}, s ps.SubcriberClient) int {
 	i := 0
 	for {
 		select {
 		case <-toggle:
 			return i
 		default:
+			s.Receive()
 			i++
 		}
 
--- workers/workers_suite_test.go
+++ workers/workers_suite_test.go
@@ -1,13 +1,14 @@
 package workers_test
 
 import (
+	"testing"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	"testing"
 )
 
 func TestWorkers(t *testing.T) {
+
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Workers Suite")
 }
